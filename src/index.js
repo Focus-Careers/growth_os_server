d3174e810c25a7e89d771cd40840d29490a13e1f
Toby <toby.king77@gmail.com>
2026-05-18 17:27:13 +0100

    revert: remove SMTP sender verification (Railway blocks outbound SMTP)

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ import campaignsRouter from './campaigns/router.js';
 import userRouter from './user/router.js';
 import itpRouter from './itp/router.js';
 import adminRouter from './admin/router.js';
-import sendersRouter from './senders/router.js';
 import { init as initCronService } from './admin/cronService.js';
 
 dotenv.config();
@@ -44,7 +43,6 @@ app.use('/api/campaigns', campaignsRouter);
 app.use('/api/user', userRouter);
 app.use('/api/itp', itpRouter);
 app.use('/api/admin', adminRouter);
-app.use('/api/senders', sendersRouter);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
